3971488a07344d9375eec5781eccf0202d0c156f
Darshu1907 <122219551+Darshu1907@users.noreply.github.com>
2023-01-30 23:11:54 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World- at 10:59pm-triggered by webhook");
+    System.out.println("Hello World - triggered by webhook");
     }
 }
